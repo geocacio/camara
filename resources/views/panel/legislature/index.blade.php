122dddae11a534fd0563c34df84caa7885989dae
Cácio Dias <cacio@gginformaticaweb.com>
2023-10-04 08:09:26 -0300

    fixing issue

--- resources/views/panel/legislature/index.blade.php
+++ resources/views/panel/legislature/index.blade.php
@@ -26,37 +26,45 @@
                         <td>{{ $legislature->id }}</td>
                         <td>{{ date('d/m/Y', strtotime($legislature->start_date)) }} - {{ date('d/m/Y', strtotime($legislature->end_date)) }}</td>
                         <td class="actions text-center">
-                            <a href="{{ route('legislatures.edit', $legislature->slug) }}" class="link edit"><i class="fas fa-edit"></i></a>
-                        
-                            <a data-toggle="modal" data-target="#myModal-{{ $legislature->id}}" class="link delete"><i class="fas fa-trash-alt"></i></a>
-                        
-                            <div id="myModal-{{$legislature->id}}" class="modal fade modal-warning" role="dialog">
-                                <div class="modal-dialog modal-dialog-centered">
-                                    <!-- Modal content-->
-                                    <div class="modal-content">
-                                        <div class="modal-body">
-                                            <span class="icon" data-v-988dbcee=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-octagon" data-v-988dbcee="">
-                                                    <polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2" data-v-988dbcee=""></polygon>
-                                                    <line x1="12" y1="8" x2="12" y2="12" data-v-988dbcee=""></line>
-                                                    <line x1="12" y1="16" x2="12.01" y2="16" data-v-988dbcee=""></line>
-                                                </svg></span>
-                                            <span class="title">Você tem certeza?</span>
-                                            <span class="message">Você realmente quer apagar este item?<br> Esta ação não poderá ser desfeita.</span>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-cancel" data-dismiss="modal">Cancelar</button>
-                                            <a href="#" class="btn btn-default" onclick="event.preventDefault();
-                                                document.getElementById('delete-form-{{ $legislature->id }}').submit();">
-                                                Deletar
-                                            </a>
+                            <div class="btn-group dropleft">
+                                <a class="link create" href="#" role="button" id="dropdownMenuLink7" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fa-solid fa-gear"></i>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" href="#">Vereadores</a>
+                                    <a class="dropdown-item" href="{{ route('legislatures.edit', $legislature->slug) }}">Editar</a>
+                                    <a class="dropdown-item" data-toggle="modal" data-target="#myModal-{{ $legislature->id }}" href="#">Excluir</a>
+                                </div>
 
-                                            <form id="delete-form-{{ $legislature->id }}" action="{{ route('legislatures.destroy', $legislature->slug) }}" method="post" style="display: none;">
-                                                @csrf
-                                                @method('DELETE')
-                                            </form>
+                                <div id="myModal-{{ $legislature->id }}" class="modal fade modal-warning" role="dialog">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                        <!-- Modal content-->
+                                        <div class="modal-content">
+                                            <div class="modal-body">
+                                                <span class="icon" data-v-988dbcee=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-octagon" data-v-988dbcee="">
+                                                        <polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2" data-v-988dbcee=""></polygon>
+                                                        <line x1="12" y1="8" x2="12" y2="12" data-v-988dbcee=""></line>
+                                                        <line x1="12" y1="16" x2="12.01" y2="16" data-v-988dbcee=""></line>
+                                                    </svg></span>
+                                                <span class="title">Você tem certeza?</span>
+                                                <span class="message">Você realmente quer apagar este item?<br> Esta ação não poderá ser desfeita.</span>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-cancel" data-dismiss="modal">Cancelar</button>
+                                                <a href="#" class="btn btn-default" onclick="event.preventDefault();
+                                                document.getElementById('delete-form-{{ $legislature->id }}').submit();">
+                                                    Deletar
+                                                </a>
+
+                                                <form id="delete-form-{{ $legislature->id }}" action="{{ route('legislatures.destroy', $legislature->slug) }}" method="post" style="display: none;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                </form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+
                             </div>
 
                         </td>
