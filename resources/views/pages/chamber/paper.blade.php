84deaa80f7a74aaa85b9342876202559640907c9
Jair Barreto <jairbarreto549@gmail.com>
2023-11-09 16:18:56 -0300

    Creating Layout PaperChamber and PaperCouncilors

--- resources/views/pages/chamber/paper.blade.php
+++ resources/views/pages/chamber/paper.blade.php
@@ -1,3 +1,37 @@
-<h3>{{ $paper->title }}</h3>
+@extends('layouts.app')
 
-<p>{!! $paper->description !!}</p>
+@section('breadcrumb')
+<ul class="breadcrumb">
+    <li class="item">
+        <a href="{{ route('home') }}" class="link">Início</a>
+    </li>
+    <li class="item">
+        <span>{{ $paper->title }}</span>
+    </li>
+</ul>
+@if($paper)
+<h3 class="title text-center">{{ $paper->title}}</h3>
+@endif
+@endsection
+
+@section('content')
+
+@include('layouts.header')
+
+<section class="section-ombudsman section-ombudsman-institutional margin-fixed-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card main-card card-manager">
+                    <p class="title">{!! $paper->description !!}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@include('pages.partials.satisfactionSurvey', ['page_name' => 'Institucional'])
+
+@include('layouts.footer')
+
+@endsection
