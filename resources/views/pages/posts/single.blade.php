099ea7cbb0b91db11ec33f3e2ddb47d995715957
Cácio Dias <cacio@gginformaticaweb.com>
2023-11-14 13:05:27 -0300

    adding video on home

--- resources/views/pages/posts/single.blade.php
+++ resources/views/pages/posts/single.blade.php
@@ -5,10 +5,10 @@
 <section>
 <div class="container">
     <div class="row">
-        <div class="col">
+        <div class="col-md-8">
             <h3 class="mb-3 text-center">{{ $post->title }}</h3>
             @if($image)
-            <img src="{{ asset('storage/'.$image->url)}}" class="mb-5" style="max-height: 300px; width: 100%; object-fit: cover">
+                <img src="{{ asset('storage/'.$image->url)}}" class="mb-5" style="max-height: 300px; width: 100%; object-fit: cover">
             @endif
             {!! $post->content !!}
         </div>
