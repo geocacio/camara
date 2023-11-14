5ba380197bba0bd57b097fddc966657ee1ce6098
Cácio Dias <cacio@gginformaticaweb.com>
2023-11-14 09:28:01 -0300

    get data ombudsman

--- resources/views/pages/home/sections/OmbudsmanListSection.blade.php
+++ resources/views/pages/home/sections/OmbudsmanListSection.blade.php
@@ -3,13 +3,15 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
+                <h3 class="title text-center mb-5">Vereadores Vigentes</h3>
                 <div class="ombudsman-carousel">
                     @foreach($currentLegislature->legislatureRelations as $legislature)
+                    
                     <a href="{{ route('vereador.single', $legislature->legislatureable->slug) }}" class="item">
                         <img class="image" src="{{ asset('storage/'.$legislature->legislatureable->files[0]->file->url) }}" alt="">
                         <div class="info">
                             <h3 class="name">{{ $legislature->legislatureable->surname }}</h3>
-                            <p class="office">Cargo</p>
+                            <p class="office">{{ $legislature->office->office }}</p>
                         </div>
                     </a>
                     @endforeach
