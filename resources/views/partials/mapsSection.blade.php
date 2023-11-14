d52a15e8c7a650c9a9cb1434f0448a9c7e810f44
Cácio Dias <cacio@gginformaticaweb.com>
2023-11-14 07:09:10 -0300

    preparing maps

--- resources/views/partials/mapsSection.blade.php
+++ resources/views/partials/mapsSection.blade.php
@@ -1,3 +1,22 @@
+@php
+$endereco = "SEU_ENDERECO_DINAMICO";
+$api_key = "SUA_CHAVE";
+
+$endereco_codificado = urlencode($endereco);
+$url = "https://maps.googleapis.com/maps/api/geocode/json?address={$endereco_codificado}&key={$api_key}";
+
+$response = file_get_contents($url);
+$data = json_decode($response);
+
+// Verifique se a solicitação foi bem-sucedida
+if ($data->status === "OK") {
+    $latitude = $data->results[0]->geometry->location->lat;
+    $longitude = $data->results[0]->geometry->location->lng;
+} else {
+    // Lida com erro na solicitação da API
+    $latitude = $longitude = null;
+}
+@endphp
 <section id="mapsSection" class="section-map no-padding">
-    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d497856.48259637057!2d-38.781993596476546!3d-12.875295371031665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x716037ca23ca5b3%3A0x1b9fc7912c226698!2sSalvador%20-%20BA!5e0!3m2!1spt-BR!2sbr!4v1686405124368!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d497856.48259637057!2d<?php echo $longitude; ?>!3d<?php echo $latitude; ?>!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x716037ca23ca5b3%3A0x1b9fc7912c226698!2sSalvador%20-%20BA!5e0!3m2!1spt-BR!2sbr!4v1686405124368!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </section>
