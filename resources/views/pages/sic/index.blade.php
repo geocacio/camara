03b7b49797b5d83eca96a23e8e527d8ff184ac50
Cácio Dias <cacio@gginformaticaweb.com>
2023-11-15 06:33:12 -0300

    fixing issue

--- resources/views/pages/sic/index.blade.php
+++ resources/views/pages/sic/index.blade.php
@@ -42,33 +42,33 @@
                 <div class="row">
                     <div class="col-md-6">
                         <p class="title">Gestor</p>
-                        <p class="description">{{ $sic->manager }}</p>
+                        <p class="description">{{ isset($sic->manager) ? $sic->manager : ''; }}</p>
                     </div>
                     <div class="col-md-6">
                         <p class="title">Telefone</p>
-                        <p class="description">{{ $sic->phone }}</p>
+                        <p class="description">{{ isset($sic->phone) ? $sic->phone : '' }}</p>
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-md-6">
                         <p class="title">E-mail</p>
-                        <p class="description">{{ $sic->email }}</p>
+                        <p class="description">{{ isset($sic->email) ? $sic->email : '' }}</p>
                     </div>
                     <div class="col-md-6">
                         <p class="title">CEP</p>
-                        <p class="description">{{ $sic->cep }}</p>
+                        <p class="description">{{ isset($sic->cep) ? $sic->cep : '' }}</p>
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-md-6">
                         <p class="title">Endereço</p>
-                        <p class="description">{{ $sic->street }} nº{{ $sic->number }} - {{ $sic->neighborhood }} {{ $sic->complement }}</p>
+                        <p class="description">{{ isset($sic->street) ? $sic->street : '' }} nº{{ isset($sic->number) ? $sic->number : '' }} - {{ isset($sic->neighborhood) ? $sic->neighborhood : '' }} {{ isset($sic->complement) ? $sic->complement : '' }}</p>
                     </div>
                     <div class="col-md-6">
                         <p class="title">Horário de atendimento</p>
-                        <p class="description">{{ $sic->opening_hours }}</p>
+                        <p class="description">{{ isset($sic->opening_hours) ? $sic->opening_hours : '' }}</p>
                     </div>
                 </div>
 
