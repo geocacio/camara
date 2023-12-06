a9539662e9dd832bb6325de7bc883ece10e6e812
widneydias <widney7311@gmail.com>
2023-12-06 20:54:33 -0300

    criando migrations de links externo

---
+++ database/migrations/2023_12_06_195426_add_column_link_in_pcgs.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pcgs', function (Blueprint $table) {
+            $table->enum('link_type', ['internal', 'external']);
+            $table->string('link_external');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pcgs', function (Blueprint $table) {
+            $table->dropColumn('link_type');
+            $table->dropColumn('link_external');
+        });
+    }
+};
