e3a869031dcb23b4981ed5e0681a45ac981be575
Cácio Dias <cacio@gginformaticaweb.com>
2024-01-09 16:56:37 -0300

    ajusta a seção do banner

--- resources/views/pages/home/sections/carrouselBanners.blade.php
+++ resources/views/pages/home/sections/carrouselBanners.blade.php
@@ -1,7 +1,11 @@
 @if($banners && $banners->count() > 0 && $section->visibility === 'enabled')
 <div class="owl-carousel carousel-header colorful">
     @foreach($banners as $banner)
+    @if(isset($banner->external_url))
+        <a class="link" href="{{ $banner->external_url }}" style="background-color: {{ $banner->color }};" target="_blank" ><img src="{{ asset('storage/'.$banner->files[0]->file->url) }}"></a>
+    @else
         <a class="link" href="{{ $banner->links ? ($banner->links->route ? route($banner->links->route) : $banner->links->url) : '#' }}" style="background-color: {{ $banner->color }};"><img src="{{ asset('storage/'.$banner->files[0]->file->url) }}"></a>
+    @endif
     @endforeach
 </div>
 @endif
