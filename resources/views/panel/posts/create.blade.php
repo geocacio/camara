13a121d3465fe2cfad6697980af63c7050e11338
widneydias <widney7311@gmail.com>
2024-02-19 13:55:17 -0300

    posts

--- resources/views/panel/posts/create.blade.php
+++ resources/views/panel/posts/create.blade.php
@@ -74,4 +74,22 @@
 
 @include('panel.scripts')
 
+
+<!-- include summernote css/js -->
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<script>
+$(document).ready(function() {
+    $('#editor').summernote({
+        placeholder: '',
+        tabsize: 2,
+        height: 100 
+    });
+});
+</script>
+
 @endsection
