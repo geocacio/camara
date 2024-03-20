98920ce31d7deb8e08e79e93c5fe2aa642265a9d
widneydias <widney7311@gmail.com>
2024-03-20 09:18:55 -0300

    ajustes em diarios

--- resources/views/panel/official-diary/page/edit.blade.php
+++ resources/views/panel/official-diary/page/edit.blade.php
@@ -54,7 +54,7 @@
 
             <div class="form-group">
                 <label>Conteúdo</label>
-                <textarea class="editor" name="featured_description">{{ old('featured_description', $outsource->featured_description) }}</textarea>
+                <textarea id="editor" name="featured_description">{{ old('featured_description', $outsource->featured_description) }}</textarea>
             </div>
 
             <div class="form-group">
@@ -94,5 +94,20 @@
 @section('js')
 
 @include('panel.scripts')
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<script>
+$(document).ready(function() {
+    $('#editor').summernote({
+        placeholder: '',
+        tabsize: 2,
+        height: 100 
+    });
+});
+</script>
 
 @endsection
