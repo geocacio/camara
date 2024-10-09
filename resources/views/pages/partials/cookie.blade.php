0ade74d27862d1afcaf8eb762ff083870d28f770
Widney Dias <widney7311@gmail.com>
2024-10-09 12:53:56 -0300

    card cookie

---
+++ resources/views/pages/partials/cookie.blade.php
@@ -0,0 +1,12 @@
+<div class="cookie-card">
+    <span class="title">🍪 Aviso de Cookies</span>
+    <p class="description">Usamos cookies para garantir que oferecemos a melhor experiência em nosso site. <a href="#">Leia as políticas de cookies</a>.</p>
+    <div class="actions">
+        <button class="pref">
+            Gerenciar suas preferências
+        </button>
+        <button class="accept">
+            Aceitar
+        </button>
+    </div>
+</div>
