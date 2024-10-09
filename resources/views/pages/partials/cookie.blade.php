d128364ccddd717323a836d26e652343d08bc015
Widney Dias <widney7311@gmail.com>
2024-10-09 13:00:56 -0300

    ocultando cookies

--- resources/views/pages/partials/cookie.blade.php
+++ resources/views/pages/partials/cookie.blade.php
@@ -1,12 +1,24 @@
-<div class="cookie-card">
+<div class="cookie-card" id="cookieCard">
     <span class="title">🍪 Aviso de Cookies</span>
     <p class="description">Usamos cookies para garantir que oferecemos a melhor experiência em nosso site. <a href="#">Leia as políticas de cookies</a>.</p>
     <div class="actions">
-        <button class="pref">
-            Gerenciar suas preferências
-        </button>
-        <button class="accept">
-            Aceitar
-        </button>
+        <button class="pref">Gerenciar suas preferências</button>
+        <button class="accept" onclick="acceptCookies()">Aceitar</button>
     </div>
 </div>
+
+<script>
+    function checkCookieConsent() {
+        const cookieConsent = localStorage.getItem('cookieConsent');
+        if (cookieConsent) {
+            document.getElementById('cookieCard').style.display = 'none';
+        }
+    }
+
+    function acceptCookies() {
+        localStorage.setItem('cookieConsent', 'accepted');
+        document.getElementById('cookieCard').style.display = 'none';
+    }
+
+    window.onload = checkCookieConsent;
+</script>
